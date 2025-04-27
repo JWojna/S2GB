f10f9faea1855bb1dee9832d149073fe452971d9
Jacob <EarthTek19@gmail.com>
2025-04-26 22:28:02 -0500

    bp login routes *tested*

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,18 @@
 //! backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-// });
-
 router.use(restoreUser)
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
 
 router.get(
     '/restore-user',
---
+++ backend/routes/api/session.js
@@ -0,0 +1,47 @@
+//! backend/routes/api/session.js
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+//! Log in
+router.post(
+    '/', async (requestAnimationFrame, restoreUser, next) => {
+        const { credential, password } = requestAnimationFrame.body;
+
+        const user = await User.unscoped().findOne({
+            where: {
+                [Op.or]: {
+                    username: credential,
+                    email: credential
+                }
+            }
+        });
+
+         if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Loging failed';
+            err.errors = { credential: 'The provided credentials were invalid'};
+            return next(err);
+         }
+
+         const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+         };
+
+         setTokenCookie(restoreUser, safeUser);
+
+         return restoreUser.json({
+            user: safeUser
+         });
+    }
+);
+
+module.exports = router;
