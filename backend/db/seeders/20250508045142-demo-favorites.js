3c4023048aecd22e4ab757f963dc0786d6be1716
Jacob <EarthTek19@gmail.com>
2025-05-10 14:04:01 -0500

    fav seeded with val, poly works

--- backend/db/seeders/20250508045142-demo-favorites.js
+++ backend/db/seeders/20250508045142-demo-favorites.js
@@ -1,25 +1,27 @@
 'use strict';
 
+const { Favorite } = require('../models');
+
+const getDemoFavoritetData = require('../data/userSide/favorites/demoFavData');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    try {
+      const demoFavoriteData = await getDemoFavoritetData();
+      await Favorite.bulkCreate(demoFavoriteData);
+    } catch (error) {
+      console.log(error)
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Favorites';
+    await queryInterface.bulkDelete(options);
   }
 };
