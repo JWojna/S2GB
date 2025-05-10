c2ce22b814ec0c6d05e24cd58dc0fd7652c9e59b
Jacob <EarthTek19@gmail.com>
2025-05-10 13:57:33 -0500

    comments seed with vals, poly working

--- backend/db/data/userSide/comments/demoCommentData.js
+++ backend/db/data/userSide/comments/demoCommentData.js
@@ -1,6 +1,53 @@
-const demoCommentData = {
-    "userId": 1,
-    "commentableType": "build",
-    "commentableId": 1,
-    "body": 'what a neat build',
-}
+module.exports = async function getDemoCommentData() {
+    const { User, Build, TierList } = require('../../../models');
+
+    const user = await User.findOne({ where: { username: 'Manny' } });
+    const userId = user?.id;
+
+    const buildOne = await Build.findOne({ where: { title: 'DEMO BUILD' } })
+    const buildOneId = buildOne?.id
+    const buildTwo = await Build.findOne({ where: { title: 'DEMO BUILD TWO' } })
+    const buildTwoId = buildTwo?.id
+
+    const tierOne = await TierList.findOne({ where: { title: 'DEMO TIER LIST' } })
+    const tierOneId = tierOne?.id
+    const tierTwo = await TierList.findOne({ where: { title: 'DEMO TIER LIST TWO' } })
+    const tierTwoId = tierTwo?.id
+
+
+
+    return [
+        {
+            "userId": userId,
+            "commentableType": "build",
+            "commentableId": buildOneId,
+            "body": 'DEMO COMMENT ON BUILD ONE',
+        },
+        //! tsting polymorph
+        {
+            "userId": userId + 1, //! im being lazy
+            "commentableType": "build",
+            "commentableId": buildOneId,
+            "body": 'DEMO COMMENT ON BUILD ONE FROM DIFF USER',
+        },
+        //! back to reg data
+        {
+            "userId": userId,
+            "commentableType": "tier",
+            "commentableId": tierOneId,
+            "body": 'DEMO COMMENT TIER ONE',
+        },
+        {
+            "userId": userId,
+            "commentableType": "build",
+            "commentableId": buildTwoId,
+            "body": 'DEMO COMMENT BUILD TWO',
+        },
+        {
+            "userId": userId,
+            "commentableType": "tier",
+            "commentableId": tierTwoId,
+            "body": 'DEMO COMMENT TIER TWO',
+        }
+    ];
+};
--- backend/db/seeders/20250508045135-demo-comments.js
+++ backend/db/seeders/20250508045135-demo-comments.js
@@ -1,25 +1,35 @@
 'use strict';
 
+const { Comment } = require('../models');
+
+const getDemoCommentData = require('../data/userSide/comments/demoCommentData');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    try {
+      const demoCommentdData = await getDemoCommentData();
+      await Comment.bulkCreate(demoCommentdData);
+    } catch (error) {
+      console.log(error)
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Comments';
+    await queryInterface.bulkDelete(options, {
+      body: [
+        'DEMO COMMENT ON BUILD ONE',
+        'DEMO COMMENT TIER ONE',
+        'DEMO COMMENT BUILD TWO',
+        'DEMO COMMENT TIER TWO',
+        'DEMO COMMENT ON BUILD ONE FROM DIFF USER'
+      ]
+    });
   }
 };
