0de40af1d90405ee675d95d2fce17665e002719b
Jacob <EarthTek19@gmail.com>
2025-05-11 18:40:04 -0500

    all get routes for Gods properly querry with assoc data

--- backend/routes/api/gods.js
+++ backend/routes/api/gods.js
@@ -17,12 +17,38 @@ router.get(
                     attributes: ['imageUrl'],
                 }]
             })
-            return res.json({Gods: gods})
+            return res.json({ Gods: gods })
         } catch (error) {
             console.error('Error fetching gods:', error);
             return res.status(500).json({ error: 'Internal Server Error' });
         }
     }
 )
+
+//! Get god details by godId
+router.get('/:godId', async (req, res) => {
+    try {
+        const { godId } = req.params;
+        const god = await God.findOne({
+            where: { godId },
+            include: [
+                {
+                    model: Image,
+                    as: 'Images',
+                    attributes: ['imageUrl']
+                },
+            ]
+        });
+
+        if (!god) {
+            return res.status(404).json({ message: `God couldn't be found` });
+        };
+
+        return res.json(god);
+    } catch (error) {
+        console.error('Error fetching god:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    };
+});
 
 module.exports = router
